63f0d2a158460335b886a9e4d350dde8ff9f11d8
Frostqui <diego.lopezgarcia@hotmail.com>
2017-08-01 19:57:09 +0200

    CORS

--- backend/app.js
+++ backend/app.js
@@ -43,6 +43,23 @@ app.use('/tasks', tasks);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
+
+     // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:9080');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+
   var err = new Error('Not Found');
   err.status = 404;
   next(err);
