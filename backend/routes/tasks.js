558c0e7027eaaed2354ca86db2d0110e25175a17
Frostqui <diego.lopezgarcia@hotmail.com>
2017-08-10 19:05:29 +0200

    Cors middleware

--- backend/routes/tasks.js
+++ backend/routes/tasks.js
@@ -6,6 +6,7 @@ var Task = require('../models/task');
 router.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
     next();
 });
 
